0e771752cbc6f52d22c723f128a9e7bd129c5ecb
ajile <v.v.milkov@gmail.com>
2014-10-20 18:44:14 +0400

    Added build command
    Fill source dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         // Конфиги сборщика документации
         jsdoc: {
             dist: {
-                src: ['public/javascript/**/*.js'], 
+                src: ['sources/javascript/**/*.js'], 
                 options: {
                     destination: 'doc',
                     template: "node_modules/grunt-jsdoc/node_modules/ink-docstrap/template",
@@ -27,9 +27,39 @@ module.exports = function(grunt) {
                     encoding: "utf-8"
                 }
             }
+        },
+
+        // Объеденяем файлы приложения вместе
+        uglify: {
+            application: {
+                files: {
+                    'public/javascript/application.min.js': [
+                        'sources/javascript/vendor/geoPosition.js',
+                        'sources/javascript/lib.js',
+                        'sources/javascript/services/user.js',
+                        'sources/javascript/services/session.js',
+                        'sources/javascript/services/connection.js',
+                        'sources/javascript/services/adapter.js',
+                        'sources/javascript/services/geolocator.js',
+                        'sources/javascript/application.js',
+                        'sources/javascript/session.js',
+                        'sources/javascript/connection.js',
+                        'sources/javascript/adapter.js',
+                        'sources/javascript/geolocator.js',
+                        'sources/javascript/router.js',
+                        'sources/javascript/controller.js',
+                        'sources/javascript/component.js',
+                        'sources/javascript/models/geopoint.js',
+                        'sources/javascript/models/user.js'
+                    ]
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['uglify']);
 }
